9a99d99ce4552ccfb579449f6bb3cb840936b135
greenr <Robert.C.Green@gmail.com>
2015-03-18 13:05:16 -0400

    Broke code down into various functions

--- PSO.py
+++ PSO.py
@@ -7,9 +7,58 @@ def fitFunc(xVals):
         fitness += xVals[i]**2 - (10*math.cos(2*math.pi*xVals[i]))
     return fitness
 
+def initPosition(Np, Nd, xMin, xMax):
+    R = [[xMin + random()*(xMax-xMin) for i in range(0, Nd)] for p in range(0, Np)]
+
+    return R
+
+def updatePosition(R, Np, Nd, xMin, xMax):
+
+     for p in range(0, Np):
+        for i in range(0, Nd):
+
+            R[p][i] = R[p][i] + V[p][i]
+
+            if R[p][i] > xMax: R[p][i] = xMax
+            if R[p][i] < xMin: R[p][i] = xMin
+
+def initVelocity(Np, Nd, vMin, vMax):
+    V =[[vMin + random()*(vMax-vMin) for i in range(0,Nd)] for p in range(0, Np)]
+
+    return V
+
+def updateVelocity(R, V, Np, Nd, j, w, vMin, vMax, chi, pBestPos, gBestPos):
+    
+    for p in range(0, Np):
+        for i in range(0, Nd):
+
+            r1 = random()
+            r2 = random()
+
+            V[p][i] = chi* (w * V[p][i] + r1*c1*(pBestPos[p][i]-R[p][i]) 
+                                        + r2*c2*(gBestPos[i]   -R[p][i]))
+
+            if V[p][i] > vMax: V[p][i] = vMax
+            if V[p][i] < vMin: V[p][i] = vMin
+
+def updateFitness(R, M, Np, pBestPos, pBestVal, gBestPos, gBestValue):
+
+    for p in range(0, Np):
+        M[p] = fitFunc(R[p])
+
+        if M[p] < gBestValue:
+            gBestValue = M[p]
+            gBestPos   = R[p]
+
+        if M[p] < pBestValue[p]:
+            pBestValue[p] = M[p]
+            pBestPos[p]   = R[p]
+
+    return gBestValue
+
 if __name__ == "__main__":
 
-    Np, Nd, Nt    = 50, 200, 10000
+    Np, Nd, Nt    = 50, 20, 100
     c1, c2        = 2.05, 2.05
     w, wMin, wMax = 0.0, 0.4, 0.9 
 
@@ -27,44 +76,16 @@ if __name__ == "__main__":
 
     history    = []
     
-    R = [[xMin + random()*(xMax-xMin) for i in range(0,Nd)] for p in range(0, Np)]
-    V = [[vMin + random()*(vMax-vMin) for i in range(0,Nd)] for p in range(0, Np)]
+    R = initPosition(Np, Nd, xMin, xMax)
+    V = initVelocity(Np, Nd, vMin, vMax)
     M = [fitFunc(R[p]) for p in range(0, Np)]
 
     for j in range(0, Nt):
 
-        # Update Position
-        for p in range(0, Np):
-            for i in range(0, Nd):
+        updatePosition(R, Np, Nd, xMin, xMax)
 
-                R[p][i] = R[p][i] + V[p][i]
-
-                if R[p][i] > xMax: R[p][i] = xMax
-                if R[p][i] < xMin: R[p][i] = xMin
-
-        # Update Fitness
-        for p in range(0, Np):
-            M[p] = fitFunc(R[p])
-
-            if M[p] < gBestValue:
-                gBestValue = M[p]
-                gBestPos   = R[p]
-                print gBestValue
-
-            if M[p] < pBestValue[p]:
-                pBestValue[p] = M[p]
-                pBestPos[p]   = R[p]
-
-        # Update Velocity
+        gBestValue = updateFitness(R, M, Np, pBestPos, pBestValue, gBestPos, gBestValue)
+        print gBestValue
+        
         w = wMax - ((wMax-wMin)/Nt)*j
-        for p in range(0, Np):
-            for i in range(0, Nd):
-
-                r1 = random()
-                r2 = random()
-
-                V[p][i] = chi* (w * V[p][i] + r1*c1*(pBestPos[p][i]-R[p][i]) 
-                                            + r2*c2*(gBestPos[i]   -R[p][i]))
-
-                if V[p][i] > vMax: V[p][i] = vMax
-                if V[p][i] < vMin: V[p][i] = vMin
+        updateVelocity(R, V, Np, Nd, j, w, vMin, vMax, chi, pBestPos, gBestPos)
