5bd2a821e0ed7e0eb335054ea885870de24e37a8
Rob Green <greenr@bgsu.edu>
2020-05-03 07:48:07 -0400

    Updates for errors

--- PSO.py
+++ PSO.py
@@ -12,51 +12,56 @@ def initPosition(Np, Nd, xMin, xMax):
 
     return R
 
-def updatePosition(R, Np, Nd, xMin, xMax):
-
-     for p in range(0, Np):
+def updatePosition(R, Np, Nd, xMin, xMax, V):
+    for p in range(0, Np):
         for i in range(0, Nd):
 
             R[p][i] = R[p][i] + V[p][i]
-
             if R[p][i] > xMax: R[p][i] = xMax
             if R[p][i] < xMin: R[p][i] = xMin
+
+    return R
 
 def initVelocity(Np, Nd, vMin, vMax):
     V =[[vMin + random()*(vMax-vMin) for i in range(0,Nd)] for p in range(0, Np)]
 
     return V
 
-def updateVelocity(R, V, Np, Nd, j, w, vMin, vMax, chi, pBestPos, gBestPos):
-    
+def updateVelocity(R, V, Np, Nd, j, w, vMin, vMax, chi, pBestPos, gBestPos, c1, c2):
+
     for p in range(0, Np):
         for i in range(0, Nd):
 
             r1 = random()
             r2 = random()
 
-            V[p][i] = chi* (w * V[p][i] + r1*c1*(pBestPos[p][i]-R[p][i]) 
-                                        + r2*c2*(gBestPos[i]   -R[p][i]))
+            V[p][i] = chi * (w * V[p][i] + r1 * c1 * (pBestPos[p][i] - R[p][i])
+                             + r2 * c2 * (gBestPos[i] - R[p][i]))
 
             if V[p][i] > vMax: V[p][i] = vMax
             if V[p][i] < vMin: V[p][i] = vMin
 
-def updateFitness(R, M, Np, pBestPos, pBestVal, gBestPos, gBestValue):
+    return V
+
+
+def updateFitness(R, M, Np, pBestPos, pBestValue, gBestPos, gBestValue):
 
     for p in range(0, Np):
         M[p] = fitFunc(R[p])
 
+        # print(M[p])         #### updated print statement format
         if M[p] < gBestValue:
             gBestValue = M[p]
-            gBestPos   = R[p]
+            print("Global Best: {}".format(gBestValue))   #### updated print statement format
+            gBestPos = R[p]
 
         if M[p] < pBestValue[p]:
             pBestValue[p] = M[p]
-            pBestPos[p]   = R[p]
+            pBestPos[p] = R[p]
 
-    return gBestValue
+    return gBestValue, gBestPos, pBestPos, pBestValue, M   #### added return for gBestPos, didn't seem to be updating
 
-if __name__ == "__main__":
+def main():
 
     Np, Nd, Nt    = 50, 20, 100
     c1, c2        = 2.05, 2.05
@@ -81,14 +86,23 @@ if __name__ == "__main__":
     M = [fitFunc(R[p]) for p in range(0, Np)]
 
     for j in range(0, Nt):
+        # print statement optional, used as a progress tracker
+        if j % 100 == 0:
+            print("Iteration #{} out of {}".format(j, Nt))
 
-        updatePosition(R, Np, Nd, xMin, xMax)
+        #### added R to make it explicit and match new return
+        R = updatePosition(R, Np, Nd, xMin, xMax, V)
+    
 
-        gBestValue = updateFitness(R, M, Np, pBestPos, pBestValue, gBestPos, gBestValue)
+        gBestValue, gBestPos, pBestPos, pBestValue, M = updateFitness(R, M, Np, pBestPos, pBestValue,
+                                                                      gBestPos, gBestValue)
         history.append(gBestValue)
         
         w = wMax - ((wMax-wMin)/Nt)*j
-        updateVelocity(R, V, Np, Nd, j, w, vMin, vMax, chi, pBestPos, gBestPos)
+        V = updateVelocity(R, V, Np, Nd, j, w, vMin, vMax, chi, pBestPos, gBestPos, c1, c2)
 
     for h in history:
-        print h
+        print(h)
+
+if __name__ == "__main__":
+    main()
